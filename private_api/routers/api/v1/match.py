99adaf09e18f672302a2c9fc77ce79dab7ab7fba
nebbyJammin <129131132+nebbyJammin@users.noreply.github.com>
2025-10-07 13:58:50 +1100

    added querying for team and matches

--- private_api/routers/api/v1/match.py
+++ private_api/routers/api/v1/match.py
@@ -1,25 +1,90 @@
-from fastapi import APIRouter, Depends, HTTPException
+from datetime import datetime
+from fastapi import APIRouter, Depends, HTTPException, Query
 from models.match import VLRMatchList
 from utils.utils import get_pool
 from models.event import VLREvent, VLREventList
 from asyncpg.pool import Pool
-from typing import Optional
+from typing import List, Optional
 from asyncpg import PostgresError
 
 router = APIRouter() # Let the v1 parent handle the prefix
 
 @router.get("/")
-async def get_matches(event_id: Optional[int] = None, page: Optional[int] = None, pool: Pool = Depends(get_pool)):
+async def get_matches(event_id: Optional[int] =
+                            Query(None, ge=0), 
+                        match_id: Optional[int] =
+                            Query(None, ge=0),
+                        stage: Optional[str] =
+                            Query(None, description="Queries matches that contains the tournament stage as a substring (case insensitive). An example of a tournament stage could be 'Group Stage' or 'Playoffs'"),
+                        tournament_round: Optional[str] =
+                            Query(None, description="Queries matches that contains the tournmaent round as a substring (case insensitive). An example of a tournament round could be 'Round 1', 'Round 2', 'Finals' or 'Lower Round 1'"),
+                        tournament_note: Optional[str] =
+                            Query(None, description="Queries matches with similar tournament notes. Most of the time, this will contain information about the match like 'Bo1', 'Bo3', '2 maps', but sometimes it may include information if the a map/entire match was forfeited."), \
+                        status: Optional[List[int]] =
+                            Query(None, description="Search by status. -1 = Unknown, 0 = Upcoming, 1 = Ongoing, 2 = Completed"), \
+                        date_start: Optional[datetime] =
+                            Query(None, description="Specify an earliest datetime (inclusive) in ISO 8601 format (without timezone data -> Implicitly using UTC time)."), \
+                        date_end: Optional[datetime] =
+                            Query(None, description="Specify a latest date (inclusive) in ISO 8601 format (without timezone data -> Implicitly using UTC time)."), \
+                        team_id: Optional[List[int]] =
+                            Query(None, description="Specify a list of team IDs to query matches that have those teams participate in the matches."), \
+                        page: Optional[int] = None, pool: Pool = Depends(get_pool)):
     LIMIT = 100
 
     query = "SELECT * FROM MATCHES WHERE TRUE"
     params = []
     i = 1
 
-    if event_id is not None:
+    if event_id:
         query += f" AND event_id = ${i}"
-        params.append(event_id)
+        params.append(f"%{event_id}%")
         i += 1
+
+    if match_id:
+        query += f" AND id = ${i}"
+        params.append(f"%{match_id}%")
+        i += 1
+
+    if stage:
+        query += f" AND stage ILIKE ${i}"
+        params.append(f"%{stage}%")
+        i += 1
+
+    if tournament_round:
+        query += f" AND tournament_round ILIKE ${i}"
+        params.append(f"%{tournament_round}%")
+        i += 1
+
+    if tournament_note:
+        query += f" AND tournament_note ILIKE ${i}"
+        params.append(f"%{tournament_note}%")
+        i += 1
+
+    if status:
+        for s in status:
+            if s < -1 or s > 2:
+                raise HTTPException(status_code=400, detail="Status must be between -1 and 2 inclusive.")
+
+        placeholders = ", ".join(f"${i+j}" for j in range(len(status)))
+        query += f" AND status in ({placeholders})"
+        params.extend(status)
+        i += len(status)
+
+    if date_start:
+        query += f" AND date_start >= ${i}"
+        params.append(date_start)
+        i += 1
+
+    if date_end:
+        query += f" AND date_start <= ${i}"
+        params.append(date_end)
+        i += 1
+
+    if team_id:
+        placeholders = ", ".join(f"${i+j}" for j in range(len(team_id)))
+        query += f" AND ( team_1_id in ({placeholders}) or team_2_id in ({placeholders}) )"
+        params.extend(team_id)
+        i += len(team_id)
 
     # Apply a limit of 500 by default
     query += f" LIMIT ${i}"
--- private_api/routers/api/v1/team.py
+++ private_api/routers/api/v1/team.py
@@ -1,21 +1,62 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from models.match import VLRMatchList
 from models.team import VLRTeamList
 from utils.utils import get_pool
 from models.event import VLREvent, VLREventList
 from asyncpg.pool import Pool
-from typing import Optional
+from typing import List, Optional
 from asyncpg import PostgresError
 
 router = APIRouter() # Let the v1 parent handle the prefix
 
 @router.get("/")
-async def get_teams(page: Optional[int] = None, pool: Pool = Depends(get_pool)):
+async def get_teams(page: Optional[int] = None, 
+                    name: Optional[str] = \
+                        Query(None, description="Searches for teams with similar team names. Use tricode to search tricode instead / in combination."), \
+                    tricode: Optional[str] = \
+                        Query(None, description="Searches for teams with similar tricodes. Use name to search for tricode instead / in combination."), \
+                    country_short: Optional[str] = \
+                        Query(None, description="Searches for teams with region name containing input. E.g. A team based in Singapore will have country_short=sg"), \
+                    country_long: Optional[str] = \
+                        Query(None, description="Searches for teams with region name containing input. This differs from country_short by searching for the full name instead of a region/country abbreviation. So Singapore will be searched as Singapore instead of sg"), \
+                    status: List[Optional[int]] = \
+                        Query(None, description="Search by status. 0 = Unknown, 1 = Inactive, 2 = Active"), \
+                    pool: Pool = Depends(get_pool)):
     LIMIT = 500
 
     query = "SELECT * FROM TEAMS WHERE TRUE"
     params = []
     i = 1
+
+    if name:
+        query += f" AND name ILIKE ${i}"
+        params.append(f"%{name}%")
+        i += 1
+
+    if tricode:
+        query += f" AND tricode ILIKE ${i}"
+        params.append(f"%{tricode}%")
+        i += 1
+
+    if country_short:
+        query += f" AND country_short ILIKE ${i}"
+        params.append(f"%{country_short}%")
+        i += 1
+
+    if country_long:
+        query += f" AND country_long ILIKE ${i}"
+        params.append(f"%{country_long}%")
+        i += 1
+
+    if status:
+        for s in status:
+            if s < 0 or s > 2:
+                raise HTTPException(status_code=400, detail="Status must be between 0 and 2 inclusive.")
+
+        placeholders = ", ".join(f"${i+j}" for j in range(len(status)))
+        query += f" AND status in ({placeholders})"
+        params.extend(status)
+        i += len(status)
 
     # Apply a limit of 500 by default
     query += f" LIMIT ${i}"
